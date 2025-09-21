ae8e097124bb891e0c04d221c03b8b6ff4590790
Lsapk <lsapk69210@gmail.com>
2025-09-21 18:03:30 +0200

    V3 erreur

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "app.deepflow"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "app.deepflow"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
