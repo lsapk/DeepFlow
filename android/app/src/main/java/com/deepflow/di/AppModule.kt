997ff77ff78ad2c4b3f77e0d790aedb99af79a51
Lsapk <lsapk69210@gmail.com>
2025-10-20 22:30:21 +0200

    V3 erreur

--- android/app/src/main/java/com/deepflow/di/AppModule.kt
+++ android/app/src/main/java/com/deepflow/di/AppModule.kt
@@ -6,6 +6,8 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.goTrue
+import io.github.jan.supabase.postgrest.postgrest
 import javax.inject.Singleton
 
 @Module
@@ -19,8 +21,9 @@ object AppModule {
             supabaseUrl = "https://xzgdfetnjnwrberyddmf.supabase.co",
             supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6Z2RmZXRuam53cmJlcnlkZG1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzMjk4MTksImV4cCI6MjA1NzkwNTgxOX0.XJFYvBiZHo1vcfCVFf79C9U6LP4Vuf05PCixBWqaYU"
         ) {
-            install(io.github.jan.supabase.gotrue.GoTrue)
-            install(io.github.jan.supabase.postgrest.Postgrest)
+
+            goTrue()
+            postgrest()
         }
     }
 }
